13f10f7c4268e314d57ce383d8842f1e0a897132
chkp-ecarbon <ecarbon@checkpoint.com>
2021-09-09 18:32:05 -0400

    Update Region

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      region = "us-east-1"
-    }
-  }
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
 }
-
 
 data "aws_ami" "ubuntu" {
   most_recent = true
